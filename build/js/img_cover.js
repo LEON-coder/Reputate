c67f42e7f4e98ab4e6ab326dbba98f9c1b7ac83a
Leonid <leonid.spb2015@yandex.ru>
2024-03-19 22:53:53 +0300

    Верстка

--- build/js/img_cover.js
+++ build/js/img_cover.js
@@ -7,40 +7,38 @@ let argumentsBlock = document.querySelector('.arguments__block')
 
 
 // Накладывание блоков с нижнего на верхний
+
 imgArguments.addEventListener('click', function() {
   if(blockFirst.classList.contains('block-visibile')) {
     blockFirst.classList.remove('block-visibile');
     } else {
-      blockFirst.classList.add('block-visibile');
+    blockSecond.classList.add('block-visibile');
     };
-})
-
+});
 
 imgArguments.addEventListener('click', function() {
   if(blockSecond.classList.contains('block-visibile')) {
     blockSecond.classList.remove('block-visibile');
     } else {
     blockSecond.classList.add('block-visibile');
-    blockFirst.classList.remove('block-visibile');
     };
 }) ;
 
-imgArguments.addEventListener('click', function() {
-  if(blockFour.classList.contains('block-visibile')) {
-    blockFour.classList.remove('block-visibile');
-    } else {
-      blockFour.classList.add('block-visibile');
-    };
-}) ;
+// imgArguments.addEventListener('click', function() {
+//   if(blockThird.classList.contains('block-visibile')) {
+//     blockThird.classList.remove('block-visibile');
+//     blockFour.classList.add('block-visibile');
+//   } else {
+//       blockThird.classList.add('block-visibile');
+//     };
+// }) ;
 
-imgArguments.addEventListener('click', function() {
-  if(blockFour.classList.contains('block-visibile')) {
-    blockFour.classList.remove('block-visibile');
-    blockThird.classList.add('block-visibile');
-    } else {
-      blockFour.classList.remove('block-visibile');
-    };
-}) ;
+// imgArguments.addEventListener('click', function() {
+//   if(blockFour.classList.contains('block-visibile')) {
+//     blockFour.classList.remove('block-visibile');
+//     blockThird.classList.add('block-visibile');
+//     }
+// }) ;
 
 
 
