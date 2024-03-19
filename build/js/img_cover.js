f74bcdaa7c0225e988e18896d1381030e22eec37
Leonid <leonid.spb2015@yandex.ru>
2024-03-19 19:41:15 +0300

    Начало верстки главной страницы

---
+++ build/js/img_cover.js
@@ -0,0 +1,54 @@
+let imgArguments = document.querySelector('.img-arguments')
+let blockFirst = document.querySelector('.block-first')
+let blockSecond = document.querySelector('.block-second')
+let blockThird = document.querySelector('.block-third')
+let blockFour = document.querySelector('.block-four');
+let argumentsBlock = document.querySelector('.arguments__block')
+
+
+// Накладывание блоков с нижнего на верхний
+imgArguments.addEventListener('click', function() {
+  if(blockFirst.classList.contains('block-visibile')) {
+    blockFirst.classList.remove('block-visibile');
+    blockSecond.classList.add('block-visibile');
+    blockThird.classList.remove('block-visibile');
+    } else {
+      blockFirst.classList.add('block-visibile');
+    };
+})
+
+
+imgArguments.addEventListener('click', function() {
+  if(blockSecond.classList.contains('block-visibile')) {
+    blockSecond.classList.remove('block-visibile');
+    blockFirst.classList.remove('block-visibile');
+    blockThird.classList.add('block-visibile');
+    } else {
+      blockSecond.classList.add('block-visibile');
+    };
+}) ;
+
+imgArguments.addEventListener('click', function() {
+  if(blockSecond.classList.contains('block-visibile')) {
+    blockSecond.classList.remove('block-visibile');
+    } else {
+      blockThird.classList.add('block-visibile');
+    };
+}) ;
+
+imgArguments.addEventListener('click', function() {
+  if(blockFour.classList.contains('block-visibile')) {
+    blockFour.classList.remove('block-visibile');
+    blockThird.classList.add('block-visibile');
+    } else {
+      blockFour.classList.remove('block-visibile');
+    };
+}) ;
+
+
+
+
+
+// function visibleBlock({
+
+// })
