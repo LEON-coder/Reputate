8efe4b7b4afc497a03d45fb2bebb70d643ae9c9a
Leonid <leonid.spb2015@yandex.ru>
2024-03-23 22:03:04 +0300

    Добавил анимации в блок Reputate умеет

--- build/js/img_cover.js
+++ build/js/img_cover.js
@@ -1,47 +1,52 @@
-let buttonSlider = document.querySelector('.button-block-slide')
+let buttonSlider = document.querySelector('.img-arguments')
 let blockFirst = document.querySelector('.block-first')
 let blockSecond = document.querySelector('.block-second')
 let blockThird = document.querySelector('.block-third')
 let blockFour = document.querySelector('.block-four');
-let argumentsBlock = document.querySelector('.arguments__block')
-
+let argumentsBlock = document.querySelector('.arguments__block');
 
 // Накладывание блоков с нижнего на верхний
 
 
-buttonSlider.addEventListener('click', function() {
-  if(blockFirst.classList.contains('block-visibile')) {
-    blockFirst.classList.remove('block-visibile');
-
-    } else {
-    blockFirst.classList.add('block-visibile');
-     blockSecond.classList.add('block--active');
-    };
+buttonSlider.addEventListener('click', function () {
+  blockFirst.classList.toggle('block-visible');
+blockSecond.classList.add('block-active ');
+  // if(blockFirst.classList.contains('.block-visible')) {
+  //   blockFirst.classList.remove('.block-visible');
+  //   argumentsBlock.style.position = "left: 0";
+  //   } else {
+  //   blockFirst.classList.add('block-visible');
+  //   };
 });
 
-buttonSlider.addEventListener('click', function() {
-  if( blockSecond.classList.add('block--active')) {
-    blockSecond.classList.remove('block-visibile');
-      blockSecond.classList.add('block-visibile');
-  } else {
-     blockSecond.classList.add('block-visibile');
-    blockThird.classList.add('block--active');
-    };
+blockFirst.addEventListener('click', function () {
+    blockSecond.classList.toggle('block-visible');
+//   if( blockSecond.classList.contains('.block-visible')) {
+//     blockFirst.classList.remove('.block-visible');
+//      blockThird.classList.add('.block-active');
+//   } else {
+//        blockSecond.classList.add('block-visible');
+//  blockThird.classList.remove('block-active');
+//     };
 }) ;
 
-// buttonSlider.addEventListener('click', function() {
-//   if(blockThird.classList.contains('block-visibile')) {
-//     blockThird.classList.remove('block-visibile');
-//   } else {
-//       blockThird.classList.add('block-visibile');
-//     };
-// }) ;
+blockSecond.addEventListener('click', function () {
+  blockThird.classList.toggle('block-visible');
+  // if(blockSecond.classList.contains('.block-visible')) {
+  //   blockSecond.classList.remove('.block-visible');
+  //    blockThird.classList.add('.block-active');
+  // } else {
+  //     blockSecond.classList.add('block-visible');
+  //   };
+}) ;
 
-// buttonSlider.addEventListener('click', function() {
-//   if(blockFour.classList.contains('block-visibile')) {
-//     blockFour.classList.remove('block-visibile');
-//   } else {
-//      blockFour.classList.add('block-visibile');
-//   }
+blockFour.addEventListener('click', function () {
+    blockThird.classList.toggle('block-visible');
+    blockFirst.classList.remove('block-visible');
+  // if(blockFour.classList.contains('.block-visible')) {
+  //   blockFour.classList.remove('.block-visible');
+  // } else {
+  //    blockFour.classList.add('block-visibile');
+  // }
 
-// }) ;
+}) ;
